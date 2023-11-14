84bf7e59ae98fb692623a82648a5b32613faf5bc
QiuzhiQiuzhiYang <woshizhangmanyu@gmail.com>
2023-11-14 17:32:46 +0800

    login页面样式布局

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,26 @@
 <!--这里是el的布局容器，整个app都在这里面-->
     <el-container>
 <!--      这里是有个置顶logo-->
-      <el-header>
-        <el-row justify="center"> <!--  justify=控制这个标签内的子标签的对齐  -->
-            <div>helloword.ai</div>
-        </el-row>
-      </el-header>
+<!--      <el-header>-->
+<!--        <el-row justify="center"> &lt;!&ndash;  justify=控制这个标签内的子标签的对齐  &ndash;&gt;-->
+<!--            <div>helloword.ai</div>-->
+<!--        </el-row>-->
+<!--      </el-header>-->
+
 <!--这里是主要的登录页面-->
       <el-main height="800px">
         <home v-if="is_login"></home>
         <login v-else ></login>
       </el-main>
 
-      <el-footer  >
+<!--      <el-footer  >-->
+<!--          <el-row justify="center">-->
+<!--            <el-col :span="24" >-->
+<!--              <Footer ></Footer>-->
+<!--            </el-col>-->
+<!--          </el-row>-->
+<!--      </el-footer>-->
 
-          <el-row justify="center">
-            <el-col :span="24" >
-              <Footer ></Footer>
-            </el-col>
-          </el-row>
-      </el-footer>
     </el-container>
 
 
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="login">
-      <el-row justify="center" align= 'middle' class="box">
-        <el-col :span="8" >
-        <div >登录框
-        </div>
-        </el-col>
-      </el-row>
+    <el-container>
+
+      <el-header  height="250px"  class="el-header">
+        <el-row >
+          <el-col :span="2" class="logo">
+            <div class="logo">helloword.ai</div>
+          </el-col>
+        </el-row>
+      </el-header>
+
+      <el-main >
+        <el-row justify="center"  >
+          <el-col :span="8" class="box" >
+            <div class="content">
+              <div class="input username">
+                <span style="color: white" class="tishi">用户名NAME:</span>
+                <input type="text" class="input" >
+              </div>
+              <div class="input username">
+                <span style="color: white" class="tishi">密码PASSWORD:</span>
+                <input type="password" class="input">
+              </div>
+            </div>
+
+            <div class="end">
+                <el-button type="primary" round class="btn" size="large">登录
+                </el-button>
+                <el-button type="primary" round class="btn" size="large">注册</el-button>
+            </div>
+          </el-col>
+        </el-row>
+      </el-main>
+
+    </el-container>
+
+
   </div>
 </template>
 
@@ -17,14 +47,59 @@
 .login{
   height: 100vh;
   background-color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 .box{
   width: 700px;
-  height: 480px;
-  background-color: #f5f5f5;
+  height: 450px;
+  background-color: rgba(5, 17, 33, 0.2);
   border-radius: 20px;
 }
+
+.el-header {
+  display: flex;
+  justify-content: center;
+  align-items: end;
+}
+.logo{
+  font-size: 50px;
+  color: #081120;
+  margin: 20px 0;
+
+}
+.input{
+  height: 80px;
+  width: 550px;
+  border-radius: 20px;
+  border: 0;
+  margin: 30px 50px ;
+  font-size: 20px;
+  font-family: "微软雅黑 Light";
+}
+.input:focus {
+  outline: 2px solid #619bf9; /* 红色实线轮廓 */
+}
+.btn{
+  width: 400px;
+  margin: 10px 10px;
+  height: 50px;
+}
+.end{
+display: flex;
+flex-direction: column;
+justify-content: end;
+  align-items: center;
+  margin-top: 60px;
+}
+.username{
+  display: flex;
+  align-items: center;
+}
+.tishi {
+  color: white;
+  font-size: 20px;
+  width: 70px;
+  margin-left: 20px;
+  margin-right: 5px;
+}
+
 </style>
